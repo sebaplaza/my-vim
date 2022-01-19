4351c69117af93deeeaeb9ba8ba54b0e5dcba25a
Sebastian Plaza <sebastianplaza@gmail.com>
2022-01-19 21:48:46 +0100

    fix: install script

--- install.sh
+++ install.sh
@@ -3,14 +3,19 @@
 REPO_BASE_URL='https://raw.githubusercontent.com/sebaplaza/my-vim/main'
 
 # Install vim-plug
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo Installing vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Download nvim config
-
-curl -flO $HOME/.config/nvim/init.vim --create-dirs $REPO_BASE_URL/init.vim
+echo Downloading nvim config
+NVIM_CONFIG_URL=$REPO_BASE_URL/init.vim
+curl --create-dirs -O --output-dir $HOME/.config/nvim/ $NVIM_CONFIG_URL
 
 # and install vim plugins
-vim -c ":PlugInstall | qa"
+echo Installing plugins
+nvim -c ":PlugInstall | qa"
 
 # Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+# echo Installing FZF
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
